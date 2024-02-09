0ff50c3634d4a873cc28c0e99b2864e66c3c943f
Yazdun <yazdun.hematlu.2020@uni.strath.ac.uk>
2024-02-09 15:08:44 +0000

    Page 140, I believe the errors should be fixed

--- src/com/craftinginterpreters/lox/Stmt.java
+++ src/com/craftinginterpreters/lox/Stmt.java
@@ -36,22 +36,21 @@ abstract class Stmt {
     final Expr expression;
     }
     static class If extends Stmt {
-        If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
-            this.condition = condition;
-            this.thenBranch = thenBranch;
-            this.elseBranch = elseBranch;
-        }
-
-        @Override
-        <R> R accept(Visitor<R> visitor) {
-            return visitor.visitIfStmt(this);
-        }
-
-        final Expr condition;
-        final Stmt thenBranch;
-        final Stmt elseBranch;
+    If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
+    this.condition = condition;
+    this.thenBranch = thenBranch;
+    this.elseBranch = elseBranch;
     }
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+    return visitor.visitIfStmt(this);
+    }
+
+    final Expr condition;
+    final Stmt thenBranch;
+    final Stmt elseBranch;
+    }
     static class Print extends Stmt {
     Print(Expr expression) {
     this.expression = expression;
